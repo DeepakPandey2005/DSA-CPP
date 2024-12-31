b51352f1c29d6853e89291c9c9bf4d9ec4f27867
Deepak <deepak481481481@gmail.com>
2024-12-31 14:08:57 +0530

    peak index code added

---
+++ Binray Search/peak_index_Mountain.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int peakIndex(vector<int>vec){
+int st=1,end=vec.size()-2;
+while(st<=end){
+    int mid=st+(end-st)/2;
+    if(vec[mid-1]<vec[mid] && vec[mid]>vec[mid+1]){
+        return mid;
+    }else if(vec[mid-1]<vec[mid]){
+        st=mid+1;
+    }else{
+        end=mid-1;
+    }
+}return -1;
+}
+int main(){
+    vector<int>vec={0,3,8,9,10,18,25,50,2};
+    int ans=peakIndex(vec);
+    cout<<"Peak index of mountain is : "<<ans;
+return 0;
+}
